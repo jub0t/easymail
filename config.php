800d0dd41244a5206efad79741b15109f86952c2
jub0t <jforeverything2007@gmail.com>
2024-03-26 16:55:01 +0100

    Update

--- config.php
+++ config.php
@@ -1,16 +1,65 @@
 <?php
 
-// Configuration for Mail server and MySQL Database
+
 include "./vendor/autoload.php";
 
 use Dallgoot\Yaml\Loader;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 $DEBUG = false;
 $YLOADER = new Loader(null, 0, $DEBUG);
 $M_CONFIG = $YLOADER->load("./config.yml")->parse();
 
-function SendMail()
+function SendMail($receivers, $message)
 {
+    global $M_CONFIG;
+
+    $mail = new PHPMailer(true);
+
+    $mail->SMTPDebug = SMTP::DEBUG_OFF;
+    $mail->SMTPAuth   = true;
+    $mail->isSMTP();
+
+    $mail->Host       = $M_CONFIG["mail"]["host"];
+    $mail->Username   = $M_CONFIG["mail"]["username"];
+    $mail->Password   = $M_CONFIG["mail"]["password"];
+    $mail->Port       = $M_CONFIG["mail"]["port"];
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+
+    $mail->setFrom($M_CONFIG["mail"]["username"], $M_CONFIG["mail"]["displayName"]);
+
+    foreach ($receivers as $key => $receiver) {
+        if (is_null($receiver["username"])) {
+            $mail->addAddress(
+                $receiver["name"]
+            );
+        } else {
+            $mail->addAddress(
+                $receiver["email"],
+                $receiver["displayName"]
+            );
+        }
+    }
+
+    // $mail->addAttachment('/var/tmp/file.tar.gz');
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+
+    $mail->isHTML(true);
+
+    if (isset($message["subject"])) {
+        $mail->Subject = $message["subject"];
+    }
+
+    if (isset($message["body"])) {
+        $mail->Body = $message["body"];
+    }
+
+    if (isset($message["altBody"])) {
+        $mail->AltBody = $message["altBody"];
+    }
+
+    $mail->send();
+    return true;
 }
---
+++ send.php
@@ -0,0 +1,30 @@
+<?php
+
+include "./config.php";
+
+header("Content-type: application/json");
+$success = false;
+$messages = [];
+
+// Logic Start
+$email_sent = SendMail(
+    [
+        [
+            "email" => "jforeverything2007@gmail.com",
+            "displayName" => "John Doe"
+        ]
+    ],
+    [
+        "body" => "<h1>Test</h1>",
+        "subject"  => "Test for email"
+    ]
+);
+// Logic End
+
+echo json_encode([
+    "success" => $success,
+    "messages" => $messages,
+    "data" => [
+        "email_sent" => $email_sent
+    ]
+]);
