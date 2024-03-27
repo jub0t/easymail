73831b72634fe49729d13b299234e583ccd98ab0
jub0t <jforeverything2007@gmail.com>
2024-03-27 11:07:05 +0100

    Update

--- sdk/test.ts
+++ sdk/test.ts
@@ -5,7 +5,7 @@ async function main() {
     const mail = new EasyMail(process.env.EM_URL as string, process.env.EM_API_KEY as string)
         .addReceiver("jforeverything2007@gmail.com")
         .setBody("This is an example")
-        .setHTMLBody("./email.html") // Overwrites the raw body
+        .setHTMLBody("./example.html") // Overwrites the raw body
         .setSubject("Node.js Sdk Test");
 
     const info = await mail.send();
